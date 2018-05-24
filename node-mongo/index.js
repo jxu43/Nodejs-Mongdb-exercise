6cc9cea3e3bea01bbaf5e99e61194b122bd109d4
JIAHAO XU <jxu43@ucsc.edu>
2018-05-24 13:34:17 +0800

    promise and callback hell

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,33 +4,40 @@ const assert = require('assert');
 const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/conFusion';
 
-MongoClient.connect(url, (err, db) => {
-    assert.equal(err,null);
+MongoClient.connect(url).then((db) => {
+
     console.log('Connected correctly to server');
 
-    //插入一个doc
-    dboper.insertDocument(db,
-        { name: "Vadonut", description: "test"}, "dishes",
-        (result) => {
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes")
+        .then((result) => {
             console.log("Insert Document:\n", result.ops);
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
 
-                dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes", (result) => {
-                        console.log("Updated Document:\n", result.result);
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes");
 
-                        dboper.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
 
-                            db.dropCollection("dishes", (result) => {
-                                console.log("Dropped Collection: ", result);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
 
-                                db.close();
-                            });
-                        });
-                });
-            });
-    });
-});
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
+
+            return db.close();
+        })
+        .catch((err) => console.log(err));
+
+})
+    .catch((err) => console.log(err));
