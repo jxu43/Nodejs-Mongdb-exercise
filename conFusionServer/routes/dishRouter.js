f65b3f0f7483f8e4ef5be1ae41cfd3678d326339
JIAHAO XU <jxu43@ucsc.edu>
2018-06-01 15:03:21 +0800

    mongoose population

--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -16,6 +16,7 @@ dishRouter.use(bodyParser.json());
 dishRouter.route('/')
 .get((req, res, next) => {
     Dishes.find({})
+        .populate('comments.author')
         .then((dishes) => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
@@ -55,6 +56,7 @@ dishRouter.route('/')
 dishRouter.route('/:dishId')
 .get((req,res,next) => {
     Dishes.findById(req.params.dishId)
+        .populate('comments.author')
         .then((dish) => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
@@ -94,6 +96,7 @@ dishRouter.route('/:dishId')
 dishRouter.route('/:dishId/comments')
 .get((req,res,next) => {
     Dishes.findById(req.params.dishId)
+        .populate('comments.author')
         .then((dish) => {
             if (dish != null) {
                 res.statusCode = 200;
@@ -112,6 +115,8 @@ dishRouter.route('/:dishId/comments')
     Dishes.findById(req.params.dishId)
         .then((dish) => {
             if (dish != null) {
+                //req.user._id is from verifeyUser, already contains the info
+                req.body.author = req.user._id;
                 dish.comments.push(req.body);
                 dish.save()
                     .then((dish) => {
@@ -163,6 +168,7 @@ dishRouter.route('/:dishId/comments')
 dishRouter.route('/:dishId/comments/:commentId')
 .get((req,res,next) => {
     Dishes.findById(req.params.dishId)
+        .populate('comments.author')
         .then((dish) => {
             if (dish != null && dish.comments.id(req.params.commentId) != null) {
                 res.statusCode = 200;
